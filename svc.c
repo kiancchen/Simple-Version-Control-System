572f133b4509b641d44d0dbdf00116593e3f25df
ed system on behalf of BH Cho 🦆 <ed>
2020-02-09 07:26:35 +0000

    Scaffold update by instructor

---
+++ svc.c
@@ -0,0 +1,69 @@
+#include "svc.h"
+
+void *svc_init(void) {
+    // TODO: Implement
+    return NULL;
+}
+
+void cleanup(void *helper) {
+}
+
+int hash_file(void *helper, char *file_path) {
+    // TODO: Implement
+    return 0;
+}
+
+char *svc_commit(void *helper, char *message) {
+    // TODO: Implement
+    return NULL;
+}
+
+void *get_commit(void *helper, char *commit_id) {
+    // TODO: Implement
+    return NULL;
+}
+
+char **get_prev_commits(void *helper, void *commit, int *n_prev) {
+    // TODO: Implement
+    return NULL;
+}
+
+void print_commit(void *helper, char *commit_id) {
+    // TODO: Implement
+}
+
+int svc_branch(void *helper, char *branch_name) {
+    // TODO: Implement
+    return 0;
+}
+
+int svc_checkout(void *helper, char *branch_name) {
+    // TODO: Implement
+    return 0;
+}
+
+char **list_branches(void *helper, int *n_branches) {
+    // TODO: Implement
+    return NULL;
+}
+
+int svc_add(void *helper, char *file_name) {
+    // TODO: Implement
+    return 0;
+}
+
+int svc_rm(void *helper, char *file_name) {
+    // TODO: Implement
+    return 0;
+}
+
+int svc_reset(void *helper, char *commit_id) {
+    // TODO: Implement
+    return 0;
+}
+
+char *svc_merge(void *helper, char *branch_name, resolution *resolutions, int n_resolutions) {
+    // TODO: Implement
+    return NULL;
+}
+
---
+++ svc.h
@@ -0,0 +1,44 @@
+#ifndef svc_h
+#define svc_h
+
+#include <stdlib.h>
+
+#define MODIFICATION 0
+#define DELETION 1
+
+typedef struct resolution {
+    char *file_name;
+    int type;
+    char *resolved_file;
+} resolution;
+
+void *svc_init(void);
+
+void cleanup(void *helper);
+
+int hash_file(void *helper, char *file_path);
+
+char *svc_commit(void *helper, char *message);
+
+void *get_commit(void *helper, char *commit_id);
+
+char **get_prev_commits(void *helper, void *commit, int *n_prev);
+
+void print_commit(void *helper, char *commit_id);
+
+int svc_branch(void *helper, char *branch_name);
+
+int svc_checkout(void *helper, char *branch_name);
+
+char **list_branches(void *helper, int *n_branches);
+
+int svc_add(void *helper, char *file_name);
+
+int svc_rm(void *helper, char *file_name);
+
+int svc_reset(void *helper, char *commit_id);
+
+char *svc_merge(void *helper, char *branch_name, resolution *resolutions, int n_resolutions);
+
+#endif
+
