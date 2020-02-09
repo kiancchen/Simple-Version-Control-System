5577f1fc308a80d16f28ef41970d176493c9a589
ed system on behalf of BH Cho 🦆 <ed>
2020-02-09 07:33:23 +0000

    Scaffold update by instructor

--- tester.c
+++ tester.c
@@ -1,9 +1,11 @@
+#include <assert.h>
 #include "svc.h"
 
 int main(int argc, char **argv) {
     void *helper = svc_init();
     
     // TODO: write your own tests here
+    // Hint: you can use assert(EXPRESSION) if you want
     
     cleanup(helper);
     
